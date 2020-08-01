442b5cb250d8698de065e8e964714466d22ad81f
unknown <aadityajoshi2699@gmail.com>
2020-08-01 17:32:34 +0530

    Login & Logout API View

--- Backend/stillinbetabackend/authentication/urls.py
+++ Backend/stillinbetabackend/authentication/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name="register"),
     path('emailverify/', VerifyEmail.as_view(), name='email_verify'),
     path('resendemail/', ResendEmail.as_view(), name='resend_email'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout')
 ]
