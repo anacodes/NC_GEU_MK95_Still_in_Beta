1b714b2b2cc0c3fc705ae92ad0cd3a4aad93e83e
unknown <aadityajoshi2699@gmail.com>
2020-08-01 16:16:32 +0530

    Added VerifyEmail & ResendEmail Urls

--- Backend/stillinbetabackend/authentication/urls.py
+++ Backend/stillinbetabackend/authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, VerifyEmail, ResendEmail
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name="register"),
+    path('emailverify/', VerifyEmail.as_view(), name='email_verify'),
+    path('resendemail/', ResendEmail.as_view(), name='resend_email'),
 ]
