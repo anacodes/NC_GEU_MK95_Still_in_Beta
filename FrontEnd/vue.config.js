47e03fc860ebf52880305ef70d287c1e32782de9
darshancool25 <lokhande.darshan25@gmail.com>
2020-08-01 15:06:58 +0530

    Added config file and moved views to src folder
    added vue.config.js file that contains configuration for all dependencies

---
+++ FrontEnd/vue.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  configureWebpack: {
+    // Set up all the aliases we use in our app.
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 6
+      })
+    ]
+  },
+  pwa: {
+    name: 'Vue Argon Dashboard',
+    themeColor: '#172b4d',
+    msTileColor: '#172b4d',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#172b4d'
+  },
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== 'production'
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.transformAssetUrls = {
+          img: 'src',
+          image: 'xlink:href',
+          'b-avatar': 'src',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'src',
+          'b-card-img-lazy': ['src', 'blank-src'],
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src'
+        }
+
+        return options
+      })
+  }
+};
