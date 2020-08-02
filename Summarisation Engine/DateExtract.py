876fec3fb8a92891409537ed5525ee112dc872c8
AbhishekJugdar <ajugdar5@gmail.com>
2020-08-02 13:19:17 +0530

    Update DateExtract.py
    Added support for another format

--- Summarisation Engine/DateExtract.py
+++ Summarisation Engine/DateExtract.py
@@ -1,18 +1,18 @@
-def chkDate(s):
+def chkDate(s, c):
     date = ""
     for x in s:
-        if x == '/' or x.isdigit():
+        if x == c or x.isdigit():
             date += x
             
-    count = date.count('/')
+    count = date.count(c)
     if count != 2:
         return ''
     
-    slashIndex = date.find('/')
-    if slashIndex+2 < len(date) and date[slashIndex+2] == '/' :
+    slashIndex = date.find(c)
+    if slashIndex+2 < len(date) and date[slashIndex+2] == c :
         return date
 
-    if slashIndex+3 < len(date) and date[slashIndex+3] == '/' :
+    if slashIndex+3 < len(date) and date[slashIndex+3] == c :
         return date
     
     return ''
@@ -20,10 +20,10 @@ def chkDate(s):
 
 def getDate(s):
     for i in range(len(s)):
-        if s[i] == '/':
+        if s[i] == '/' or s[i] == '-':
             l = max(0, i - 5)
             r = min(len(s) - 1, i + 10)
-            res = chkDate(s[l:r+1])
+            res = chkDate(s[l:r+1], s[i])
 
             if res != '':
                 return res
