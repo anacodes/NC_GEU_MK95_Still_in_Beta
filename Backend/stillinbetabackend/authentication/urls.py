a153fb870b2b151f66554d50f36a190e02666252
unknown <aadityajoshi2699@gmail.com>
2020-08-02 08:15:14 +0530

    Applicant App created

--- Backend/stillinbetabackend/authentication/urls.py
+++ Backend/stillinbetabackend/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmail, ResendEmail
+from .views import RegisterView, VerifyEmail, ResendEmail, RequestPasswordResetEmailAPI, PasswordTokenCheckAPI, LoginAPIView, LogoutAPIView, SetNewPassword
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name="register"),
