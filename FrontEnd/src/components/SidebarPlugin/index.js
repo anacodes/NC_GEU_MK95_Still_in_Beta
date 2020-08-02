00fb653be056a6713577c67ed96f5c976c5b93a5
Yashodhan Agnihotri <work.yashodhaniiitp@gmail.com>
2020-08-02 14:27:20 +0530

    Added Sidebar and related

---
+++ FrontEnd/src/components/SidebarPlugin/index.js
@@ -0,0 +1,29 @@
+import Sidebar from './SideBar.vue';
+import SidebarItem from './SidebarItem.vue';
+
+const SidebarStore = {
+  showSidebar: false,
+  sidebarLinks: [],
+  isMinimized: false,
+  displaySidebar(value) {
+    this.showSidebar = value;
+  }
+};
+
+const SidebarPlugin = {
+  install(Vue, options) {
+    if (options && options.sidebarLinks) {
+      SidebarStore.sidebarLinks = options.sidebarLinks;
+    }
+    let app = new Vue({
+      data: {
+        sidebarStore: SidebarStore
+      }
+    });
+    Vue.prototype.$sidebar = app.sidebarStore;
+    Vue.component('side-bar', Sidebar);
+    Vue.component('sidebar-item', SidebarItem);
+  }
+};
+
+export default SidebarPlugin;
