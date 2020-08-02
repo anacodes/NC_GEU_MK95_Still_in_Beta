5caedf396b168886a307e39bbb467cd0cb610437
unknown <aadityajoshi2699@gmail.com>
2020-08-02 10:59:46 +0530

    Recruiter Created and serializers added to applicant

--- Backend/stillinbetabackend/stillinbetabackend/settings.py
+++ Backend/stillinbetabackend/stillinbetabackend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'authentication',
     'applicants',
+    'recruiter',
     'corsheaders',
     'django_cleanup.apps.CleanupConfig',
 ]
