b58b581a4daaa18bebf840a3c83d6ce5d932ed55
unknown <aadityajoshi2699@gmail.com>
2020-08-02 14:56:46 +0530

    Commented part of core and added views in main file

--- Backend/stillinbetabackend/stillinbetabackend/urls.py
+++ Backend/stillinbetabackend/stillinbetabackend/urls.py
@@ -21,4 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-]
+    path('applicant/', include('applicants.urls')),
+    path('recruiter/', include('recruiter.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
