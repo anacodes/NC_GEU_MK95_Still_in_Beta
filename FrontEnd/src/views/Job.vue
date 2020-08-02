d594851704b4eaf3fb3b4014118359fac4b79595
Yashodhan Agnihotri <work.yashodhaniiitp@gmail.com>
2020-08-02 12:56:02 +0530

    Added Job creation pages

---
+++ FrontEnd/src/views/Job.vue
@@ -0,0 +1,172 @@
+<template>
+  <div>
+    <base-header type="gradient-info" class="pb-4 pt-5 pt-md-8"></base-header>
+
+    <div class="container-fluid mt--7">
+      <div class="row">
+        <div class="col-xl-8 order-xl-2 mb-5 mb-xl-0 mx-auto">
+          <card
+            type="secondary"
+            shadow
+            header-classes="bg-white"
+            class="border-0 mb-4 mt-sm-5 mt-md-5 mt-lg-2"
+          >
+            <div class="border-bottom pb-2">
+              <div class="row">
+                <div class="col-md-9 col-lg-6">
+                  <h1>{{job.title}}</h1>
+                  <br />
+                  <h3>
+                    <i class="ni ni-pin-3"></i>
+                    {{job.company_name}} {{job.location}}
+                  </h3>
+                  <br />
+                </div>
+                <div class="col-8 col-sm-7 col-md-5 col-lg-6 text-right">
+                  <b-button variant="outline-success" class="my-2" @click="apply()">Apply</b-button>
+                </div>
+              </div>
+              <div class="row mt-3">
+                <div class="col-lg-4 col-md-4 col-sm-6 mt-3 text-center">
+                  <h2>
+                    <i class="ni ni-calendar-grid-58"></i> Deadline
+                  </h2>
+                  <br />
+                  {{job.deadline}}
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 mt-3 text-center">
+                  <h2>
+                    <i class="ni ni-credit-card"></i> Payscale
+                  </h2>
+                  <br />
+                  {{job.payscale}}
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 mt-3 text-center">
+                  <h2>
+                    <i class="ni ni-single-copy-04"></i> Vacancies
+                  </h2>
+                  <br />
+                  {{job.vacancy}}
+                </div>
+              </div>
+            </div>
+            <div class="mt-3 ml-1">
+              <div class="mb-3">
+                <div>
+                  <h2>About the company</h2>
+                </div>
+                <router-link
+                  :to="{ name: 'company', params: { name:  job.company_display }}"
+                  target="_blank"
+                >{{job.company_display}}</router-link>
+              </div>
+
+              <div class="mb-3">
+                <div>
+                  <h2>Skills</h2>
+                </div>
+                <div>{{job.skills}}</div>
+              </div>
+
+              <div class="mb-3">
+                <div>
+                  <h2>Summary</h2>
+                </div>
+                <p>
+                  <span v-html="job.summary"></span>
+                </p>
+              </div>
+              <div class="text-center">
+                <b-button
+                  variant="outline-primary"
+                  class="my-2"
+                  @click="checkJD()"
+                >Check Original Job Description</b-button>
+              </div>
+            </div>
+          </card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+// Tables
+import SocialTrafficTable from "./Dashboard/SocialTrafficTable";
+import PageVisitsTable from "./Dashboard/PageVisitsTable";
+export default {
+  name: "job_jobseeker",
+  props: {
+    myJob: Object
+  },
+  created() {
+    this.checkData();
+  },
+  data() {
+    return {
+      job: {}
+    };
+  },
+  methods: {
+    checkData() {
+      if (!this.myJob) {
+        alert("not recieved job");
+        this.$router.push("/jobseeker");
+      } else {
+        // console.log(this.myJob);
+        this.job.jobid = this.myJob.jobid;
+        this.job.title = this.myJob.job_title;
+        this.job.location = this.myJob.location;
+        this.job.deadline = this.myJob.deadline;
+        this.job.summary = this.myJob.summary;
+        this.job.skills = this.myJob.skills;
+        this.job.vacancy = this.myJob.total_vacancy;
+        this.job.jd = this.myJob.jd;
+        this.job.company_name = this.myJob.company.user.name;
+        this.job.payscale =
+          this.myJob.salary === 0 ? "not avaiable" : this.myJob.salary;
+        this.job.company_display = this.myJob.company.display_name;
+      }
+    },
+    checkJD: function() {
+      window.open(this.job.jd, "_blank");
+    },
+
+    apply() {
+      this.$store
+        .dispatch("APPLYJOB", {
+          jobid: this.job.jobid
+        })
+        .then(success => {
+          var msg = "Hi you've succesfully applied ";
+          this.$swal(msg, "check your dashboard", "success");
+          this.$router.push("/jobseeker");
+        })
+        .catch(error => {
+          console.log(error.data);
+          this.errors.push(...error.data);
+        });
+    }
+  }
+};
+</script>
+<style>
+h1,
+h2,
+h3 {
+  display: inline;
+}
+
+h2 {
+  font-weight: 600;
+  line-height: 1.2;
+}
+
+.table td {
+  font-size: 1.1rem;
+}
+
+.table thead th {
+  font-size: 1.1rem;
+}
+</style>
---
+++ FrontEnd/src/views/JobCreate.vue
@@ -0,0 +1,248 @@
+<template>
+  <div>
+    <base-header class="header pb-8 pt-7 d-flex align-items-center">
+      <span class="mask bg-gradient-info opacity-8"></span>
+      <div class="container-fluid align-items-center">
+        <div class="row">
+          <div class="col-md-10">
+            <h1 class="display-3 text-white">Post a Job here</h1>
+            <p class="text-white my-2">Upload the JD, and we'll handle the rest. &#128521;</p>
+            <p class="text-white mb-2">You can always add more details, if you feel like.</p>
+          </div>
+        </div>
+      </div>
+    </base-header>
+
+    <div class="container-fluid mt--7">
+      <div class="row">
+        <div class="col-xl-10 order-xl-1">
+          <card shadow type="secondary">
+            <div slot="header" class="bg-white border-0">
+              <div class="row align-items-center">
+                <div class="col-8">
+                  <h3 class="mb-0">Job - Create</h3>
+                </div>
+              </div>
+            </div>
+            <template>
+              <form @submit.prevent>
+                <h6 class="heading-small text-muted mb-4">Job Description (JD)</h6>
+                <div class="pl-lg-4">
+                  <div class="row">
+                    <div class="col-lg-12">
+                      <h4>Upload Job Description</h4>
+                      <p></p>
+                      <input type="file" id="file" ref="file" v-on:change="onJDupload()" />
+                    </div>
+                    <div class="text-center">
+                      <base-button
+                        type="success"
+                        nativeType="submit"
+                        class="my-4 mx-2"
+                        @click="onSubmitJD()"
+                        disabled
+                      >Submit JD</base-button>
+                    </div>
+                  </div>
+                </div>
+                <hr class="my-4" />
+
+                <h6 class="heading-small text-muted mb-4">Additional Information</h6>
+                <div class="pl-lg-4">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <base-input
+                        v-model="email"
+                        alternative
+                        label="Email"
+                        placeholder="hrmanager@gmail.com"
+                        input-classes="form-control-alternative"
+                      />
+                    </div>
+                    <div class="col-md-6">
+                      <base-input
+                        v-model="jobtitle"
+                        alternative
+                        label="Job Title"
+                        placeholder="e.g Software Engineer"
+                        input-classes="form-control-alternative"
+                      />
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <base-input
+                        v-model="location"
+                        alternative
+                        label="Location"
+                        placeholder="e.g Dehradun"
+                        input-classes="form-control-alternative"
+                      />
+                    </div>
+                    <div class="col-md-6">
+                      <base-input
+                        v-model="vacancy"
+                        alternative
+                        label="Vacancies"
+                        placeholder="e.g 3400"
+                        input-classes="form-control-alternative"
+                      />
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <base-input
+                        v-model="salary"
+                        alternative
+                        label="Salary"
+                        placeholder="e.g $5000"
+                        input-classes="form-control-alternative"
+                      />
+                    </div>
+                    <div class="col-lg-6">
+                      <h4>Deadline</h4>
+                      <base-input addon-left-icon="ni ni-calendar-grid-58">
+                        <flat-picker
+                          slot-scope="{focus, blur}"
+                          @on-open="focus"
+                          @on-close="blur"
+                          :config="{allowInput: true}"
+                          class="form-control datepicker"
+                          placeholder=" Select date"
+                          v-model="deadline"
+                        ></flat-picker>
+                      </base-input>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <base-input
+                        v-model="domain"
+                        alternative
+                        label="Domain"
+                        placeholder="e.g IT"
+                        input-classes="form-control-alternative"
+                      />
+                    </div>
+                    <div class="col-md-3">
+                      <h4> Activation Options</h4>
+                      <b-form-radio v-model="picked" name="radios" value="false">Draft Only</b-form-radio>
+                      <b-form-radio v-model="picked" name="radios" value="true">Activate</b-form-radio>
+                    </div>
+                    <div class="col-md-3">
+                      <h4> Job Type</h4>
+                      <b-form-radio v-model="jytpe" name="radio" value="false">Part-Time</b-form-radio>
+                      <b-form-radio v-model="jtype" name="radio" value="true">Full-Time</b-form-radio>
+                      <b-form-radio v-model="jtype" name="radio" value="false">Internship</b-form-radio>
+                    </div>
+                  </div>
+                </div>
+
+                <hr class="my-4" />
+                <h6 class="heading-small text-muted mb-4">Job Summary</h6>
+                <div class="pl-lg-4">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <vue-editor v-model="content" value="Hi, bhenchod"></vue-editor>
+                    </div>
+                  </div>
+                </div>
+                <div class="pl-lg-4">
+                  <br />
+                  <div>
+                    <div class="text-center">
+                      <base-button
+                        type="primary"
+                        nativeType="submit"
+                        class="my-4"
+                        @click="uploadJob()"
+                      >Upload Job</base-button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </template>
+          </card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { VueEditor } from "vue2-editor";
+import Datepicker from "vuejs-datepicker";
+import flatPicker from "vue-flatpickr-component";
+import "flatpickr/dist/flatpickr.css";
+export default {
+  components: {
+    VueEditor,
+    Datepicker,
+    flatPicker
+  },
+
+  data() {
+    return {
+      errors: [],
+      content: "",
+      deadline: "",
+      email: "",
+      vacancy: "",
+      salary: "",
+      jobtitle: "",
+      location: "",
+      picked: "true",
+      jtype:"true",
+      jd: "",
+      skills: "nothing for now"
+    };
+  },
+  methods: {
+    onJDupload() {
+      this.jd = this.$refs.file.files[0];
+      // console.log("zala ka upload",  this.$refs.file.files)
+    },
+    onSubmitJD() {},
+    uploadJob() {
+      this.errors = [];
+
+      if (this.valid()) {
+        const fd = new FormData();
+        fd.append("email_id", this.email);
+        fd.append("skills", this.skills);
+        fd.append("job_title", this.jobtitle);
+        fd.append("location", this.location);
+        fd.append("deadline", this.deadline);
+        fd.append("total_vacancy", this.vacancy);
+        fd.append("salary", this.salary);
+        fd.append("jd", this.jd);
+        fd.append("summary", this.content);
+        fd.append("status", true);
+        fd.append("activate", this.picked);
+        // console.log(fd);
+
+        this.$store
+          .dispatch("JOBCREATE", fd)
+          .then(success => {
+            var msg = "successfuly changed changes";
+            this.$swal(msg, "Thank You!", "success");
+            this.$router.push("/recruiter");
+          })
+          .catch(error => {
+            console.log(error.data);
+            this.errors.push(error.data);
+          });
+      }
+    },
+    valid() {
+      if (!this.errors.length) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    uploadJD() {}
+  }
+};
+</script>
+<style>
+</style>
